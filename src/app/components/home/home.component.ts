521b6400e77dedb62858d2f4ff0ae36f510bb5b4
Aniruddha Das <krenteservices@gmail.com>
2026-05-11 00:00:39 -0700

    initial site is working

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component'; // Import NavbarComponent
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NavbarComponent], // Add NavbarComponent here
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private users = [
     { id: 1, name: 'Test User', email: 'test@example.com' },
-    { id: 2, name: 'Property Owner', email: 'owner@example.com' }
+    { id: 2, name: 'Property Owner', email: 'owner@example.com' },
   ];
 
   getUserById(id: number) {
-    return this.users.find(u => u.id === id);
+    return this.users.find((u) => u.id === id);
+  }
+  updateUser(id: number) {
+    return this.users.find((u) => u.id === id);
   }
 }
